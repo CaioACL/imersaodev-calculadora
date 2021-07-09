36f5319f604efa1fec1ec0c99db26a54794cd7bf
Caio Azevedo <87204701+CaioACL@users.noreply.github.com>
2021-07-09 20:23:47 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
-var primVal = parseInt(prompt("Digite o primeiro valor:"))
+var primVal = parseInt(prompt("Digite o primeiro valor:"));
 
-var segVal = parseInt(prompt("Digite o segundo valor:"))
+var segVal = parseInt(prompt("Digite o segundo valor:"));
 
-
-var operacao = prompt('Escolha a operação. 1: Divisão, 2: Multiplicação, 3: Soma, 4: Subtração.')
+var operacao = prompt(
+  "Escolha a operação. 1: Divisão, 2: Multiplicação, 3: Soma, 4: Subtração."
+);
 
 if (operacao == 1) {
-  var resultado = primVal / segVal
-  document.write("<h2>" + primVal + " / " + segVal + " = " + resultado + "</h2>")
+  var resultado = primVal / segVal;
+  document.write(
+    "<h2>" + primVal + " / " + segVal + " = " + resultado + "</h2>"
+  );
 } else if (operacao == 2) {
-  var resultado = primVal * segVal
-  document.write("<h2>" + primVal + " X " + segVal + " = " + resultado + "</h2>")
+  var resultado = primVal * segVal;
+  document.write(
+    "<h2>" + primVal + " X " + segVal + " = " + resultado + "</h2>"
+  );
 } else if (operacao == 3) {
-  var resultado = primVal + segVal
-  document.write("<h2>" + primVal + " + " + segVal + " = " + resultado + "</h2>")
+  var resultado = primVal + segVal;
+  document.write(
+    "<h2>" + primVal + " + " + segVal + " = " + resultado + "</h2>"
+  );
 } else if (operacao == 4) {
-  var resultado = primVal - segVal
-  document.write("<h2>" + primVal + " - " + segVal + " = " + resultado + "</h2>")
+  var resultado = primVal - segVal;
+  document.write(
+    "<h2>" + primVal + " - " + segVal + " = " + resultado + "</h2>"
+  );
 } else {
-  document.write("<h2>Opção Inválida</h2>")
+  document.write("<h2>Opção Inválida</h2>");
 }
